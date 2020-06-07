4af6e46ebbaf3748058221bfcf736360e0ff20ae
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2020-06-07 04:55:14 -0500

    First commit

---
+++ init.zsh
@@ -0,0 +1,41 @@
+# paths
+ZSH_CACHE_DIR="${ZSH}/cache"
+ZSH_CONF_DIR="${ZSH}/conf"
+ZSH_PLUGIN_DIR="${ZSH}/plugins"
+ZSH_THEME_DIR="${ZSH}/themes"
+
+
+# setup completion
+autoload -U compaudit compinit
+
+if [ -z "$ZSH_COMPDUMP" ]
+then
+  ZSH_COMPDUMP="${ZSH_CACHE_DIR}/completions.db"
+fi
+compinit -i -C -d "${ZSH_COMPDUMP}"
+
+
+# load plugins
+for plugin in $plugins
+do
+  plugdir=${ZSH_PLUGIN_DIR}/$plugin
+  for script in $plugdir/$plugin.plugin.zsh $plugdir/$plugin.zsh $plugdir/$plugin.sh
+  do
+    if [[ -f $script ]]
+    then
+      source $script
+      break
+    fi
+  done
+done
+unset plugin plugdir script
+
+
+# load config files
+for config in $(ls $ZSH_CONF_DIR/*.zsh-conf)
+do
+  source $config
+done
+unset config
+
+# vim: set ft=zsh ts=2 sw=0 et:
---
+++ zshrc
@@ -0,0 +1,31 @@
+#!/usr/bin/zsh
+
+# set path to zsh directory
+export ZSH=$HOME/.zsh
+
+# set prompt theme
+ZSH_THEME="antsy"
+
+# define plugins
+typeset -a plugins
+plugins+=(zsh-completions)
+plugins+=(zsh-autosuggestions)
+plugins+=(zsh-autopair)
+plugins+=(z)
+plugins+=(fz)
+plugins+=(forgit)
+plugins+=(fast-syntax-highlighting)
+plugins+=(you-should-use)
+plugins+=(auto-notify)
+plugins+=(autoswitch_virtualenv)
+
+# load init script
+source ${ZSH}/init.zsh
+
+# basics
+export PAGER=less
+export EDITOR=vim
+export DIFFPROG=vimdiff
+export LESS='-RFW'
+
+# vim: set ft=zsh ts=2 sw=0 et:
